d70d31b57e2fe16610f97c1efbf6816eed68056e
Henil <hnkakadiya2705@gmail.com>
2021-06-11 20:08:08 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const app = express();
+
+const PORT = 8000;
+app.get('/', (req, res) => {
+    res.send('Server is running')
+});
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+})
